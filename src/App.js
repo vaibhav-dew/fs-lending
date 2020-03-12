1626c09bc24a043607dc1bc247d6e91b8582c8ab
vaibhav-dew <vaibhavv@dewsolutions.in>
2020-03-12 10:44:39 +0530

    restructuring

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./containers/home/Home";
-import ConfirmLimit from "./containers/credit-line/landing-page/LandingPage";
+import LandingPage from "./containers/credit-line/landing-page/LandingPage";
+import PanDetails from "./containers/credit-line/pan-details/PanDetails";
 import ConfirmLimit from "./containers/credit-line/confirm-limit/ConfirmLimit";
 
 const App = () => {
@@ -10,7 +11,7 @@ const App = () => {
       <Fragment>
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/credit-line/" component={LandingPage} />
+          <Route exact path="/credit-line/" component={LandingPage} />
           <Route path="/credit-line/pan-details" component={PanDetails} />
           <Route path="/credit-line/confirm-limit" component={ConfirmLimit} />
         </Switch>
