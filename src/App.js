ba64e58a7388c9c5027789ee0bf5b875b92d0011
vaibhav-dew <vaibhavv@dewsolutions.in>
2020-03-12 11:06:44 +0530

    adding pan

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./containers/home/Home";
+import LandingPage from "./containers/credit-line/landing-page/LandingPage";
+import PanDetails from "./containers/credit-line/pan-details/PanDetails";
+import ConfirmLimit from "./containers/credit-line/confirm-limit/ConfirmLimit";
 
 const App = () => {
   return (
@@ -7,13 +11,17 @@ const App = () => {
       <Fragment>
         <Switch>
           <Route exact path="/">
-            Home
+            <Home />
           </Route>
           <Route exact path="/credit-line/">
-            LandingPage
+            <LandingPage />
           </Route>
-          <Route path="/credit-line/pan-details">PanDetails</Route>
-          <Route path="/credit-line/confirm-limit">ConfirmLimit</Route>
+          <Route path="/credit-line/pan-details">
+            <PanDetails />
+          </Route>
+          <Route path="/credit-line/confirm-limit">
+            <ConfirmLimit />
+          </Route>
         </Switch>
       </Fragment>
     </Router>
