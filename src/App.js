3af9dcddf3ee27eb9828726ce39abe674f8c1a34
vaibhav-dew <vaibhavv@dewsolutions.in>
2020-03-12 11:02:09 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./containers/home/Home";
-import LandingPage from "./containers/credit-line/landing-page/LandingPage";
-import PanDetails from "./containers/credit-line/pan-details/PanDetails";
-import ConfirmLimit from "./containers/credit-line/confirm-limit/ConfirmLimit";
 
 const App = () => {
   return (
     <Router>
       <Fragment>
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/credit-line/" component={LandingPage} />
-          <Route path="/credit-line/pan-details" component={PanDetails} />
-          <Route path="/credit-line/confirm-limit" component={ConfirmLimit} />
+          <Route exact path="/">
+            Home
+          </Route>
+          <Route exact path="/credit-line/">
+            LandingPage
+          </Route>
+          <Route path="/credit-line/pan-details">PanDetails</Route>
+          <Route path="/credit-line/confirm-limit">ConfirmLimit</Route>
         </Switch>
       </Fragment>
     </Router>
--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <ul>
+        <li>
+          <Link to="/credit-line">Credit Line</Link>
+        </li>
+        <li>
+          <Link to="/credit-line/pan-details">Pan Details</Link>
+        </li>
+        <li>
+          <Link to="/credit-line/confirm-limit">Confirm Limit</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
