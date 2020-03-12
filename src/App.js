0d24c212a0f02b618ebb2b879a8b9667acb52faf
Harsh <harshg@dewsolutions.in>
2020-03-12 10:29:52 +0530

    landing page added style added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./containers/home/Home";
+import LandingPage from "./containers/CreditLine/LandingPage"
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Fragment>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/creditline" component={LandingPage}></Route>
         </Switch>
       </Fragment>
     </Router>
---
+++ src/containers/CreditLine/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {HeaderContainer} from './Style'
+
+function Header(){
+    return(
+        <React.Fragment>
+            <HeaderContainer>
+                Credit Line
+            </HeaderContainer>
+        </React.Fragment>
+    )
+}
+
+export default Header;
