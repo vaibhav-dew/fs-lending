ddb150e66bde45e33a0afb654290895598828a7b
vaibhav-dew <vaibhavv@dewsolutions.in>
2020-03-17 18:36:10 +0530

    adding navbar

---
+++ src/components/navbar/Navbar.style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const CreditLineNav = styled.nav`
+  text-align: center;
+  background-color: #dcdcdc;
+  padding: 0.75rem;
+`;
+
+export const CreditLineNavTitle = styled.div`
+  font-family: sans-serif;
+  font-size: 1.25rem;
+`;
