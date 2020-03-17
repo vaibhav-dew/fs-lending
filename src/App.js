c64bcdb7c1f1e13c7a0c21bc7655f6f02daf7308
vaibhav-dew <vaibhavv@dewsolutions.in>
2020-03-17 22:46:56 +0530

    optimizing stuff

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./containers/home/Home";
 import LandingPage from "./containers/credit-line/landing-page/Index";
 import PanDetails from "./containers/credit-line/pan-details/Index";
 import ConfirmLimit from "./containers/credit-line/confirm-limit/ConfirmLimit";
-import KycDetails from "./containers/credit-line/Kyc-details/KycDetails"
+import KycDetails from "./containers/credit-line/Kyc-details/KycDetails";
 const App = () => {
   return (
-      <Fragment>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/credit-line/" component={LandingPage}/>
-            {/* <LandingPage />
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/credit-line/" component={LandingPage} />
+        {/* <LandingPage />
           </Route> */}
-          <Route path="/credit-line/pan-details" component={PanDetails}  />
-            {/* <PanDetails />
+        <Route path="/credit-line/pan-details" component={PanDetails} />
+        {/* <PanDetails />
           </Route> */}
-          <Route path="/credit-line/confirm-limit" component={ConfirmLimit}/>
-            {/* <ConfirmLimit />
+        <Route path="/credit-line/confirm-limit" component={ConfirmLimit} />
+        {/* <ConfirmLimit />
           </Route> */}
-          <Route path="/credit-line/kyc-details" component={KycDetails}  />
-        </Switch>
-      </Fragment>
+        <Route path="/credit-line/kyc-details" component={KycDetails} />
+      </Switch>
+    </Router>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import {  Provider } from 'react-redux';
-import {createStore, applyMiddleware , combineReducers} from 'redux';
-import logger from 'redux-logger'
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 import panVerificationReducer from "./containers/credit-line/pan-details/Reducer";
 import { BrowserRouter as Router } from "react-router-dom";
 
 // const rootReducer = combineReducers({
 //     panVerificationReducer:panVerificationReducer
 // })
-console.log(panVerificationReducer,'panVerificationReducer')
-const store= createStore(panVerificationReducer,composeWithDevTools(applyMiddleware(logger,thunk)))
-ReactDOM.render(<Router>
+console.log(panVerificationReducer, "panVerificationReducer");
+const store = createStore(
+  panVerificationReducer,
+  composeWithDevTools(applyMiddleware(logger, thunk))
+);
+ReactDOM.render(
+  <Router>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
-</Router>, document.getElementById("root"));
+  </Router>,
+  document.getElementById("root")
+);
