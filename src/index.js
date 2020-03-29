8d1e9c8769774f177a88da20af0c6739a152912f
vaibhav-dew <vaibhavv@dewsolutions.in>
2020-03-29 15:19:27 +0530

    changes in state and removing redux logger

--- src/index.js
+++ src/index.js
@@ -3,19 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
-import panVerificationReducer from "./containers/credit-line/pan-details/Reducer";
+import rootReducer from "./containers/credit-line/Reducer";
 import { BrowserRouter as Router } from "react-router-dom";
 
 // const rootReducer = combineReducers({
 //     panVerificationReducer:panVerificationReducer
 // })
-console.log(panVerificationReducer, "panVerificationReducer");
 const store = createStore(
-  panVerificationReducer,
-  composeWithDevTools(applyMiddleware(logger, thunk))
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
 );
 ReactDOM.render(
   <Router>
