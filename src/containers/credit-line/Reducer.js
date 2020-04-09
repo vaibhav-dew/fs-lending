bb1789c20d36ea65e88254f405ee9d73fb96d5ea
vaibhav-dew <vaibhavv@dewsolutions.in>
2020-04-09 13:42:10 +0530

    adding onboarding form in redux

--- src/containers/credit-line/Reducer.js
+++ src/containers/credit-line/Reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import panVerificationReducer from "./pan-details/Reducer";
 import personalDetails from "./PersonalDetails/Reducer";
+import onboardingForm from "./OnboardingForm/Reducer";
 
 const rootReducer = combineReducers({
   personalDetails,
-  panVerificationReducer
+  panVerificationReducer,
+  onboardingForm,
 });
 
 export default rootReducer;
